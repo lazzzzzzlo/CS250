2aa678364a57d2b08d4e969ddce7e371fabd418e
Leonardo Azopardo <lazopard@gmail.com>
2014-04-30 19:02:17 -0400

    more progress done

--- lab13/connect4.s
+++ lab13/connect4.s
@@ -1,4 +1,4 @@
-#Connect 4
+/* -- Connect 4 -- */
 
 .data
 
@@ -9,10 +9,13 @@ board: .ascii "| - - - - - - - |\n| - - - - - - - |\n| - - - - - - - |\n| - - - 
 
 #Gameplay
 invalid_move: .asciiz "Invalid move. Try again."
-move_prompt: .asciiz "Player %d choose column:"
+move_prompt: .asciiz "Player   choose column:"
+
+#Column
+col: .comm 1
 
 #End game
-win: .asciiz "Player %d Wins.\n"
+win: .asciiz "Player   Wins.\n"
 tie: .asciiz "Tie Game."
 
 #Exiting
@@ -22,20 +25,70 @@ exiting: .asciiz "----- Exiting Connect 4 -----\n"
 raw: .ascii "stty raw"
 cooked: .ascii "stty cooked"
 
-/* -- Code */
 .text
 
+.global move_valid
+.func move_valid
+
+move_valid:
+    bx lr
+
 .global play_turn
+.func play_turn
+
+/* Args: r0:board, r1:player
+ * Prompt player for input
+ * check if move is valid
+ * if valid add checker to 
+ * right column 
+ */
 
 play_turn:
+
+    str r1, [r0, +#7]
+    ldr r1, r0
+    mov r0, #1
+    mov r2, #23
+    bl write
+
+    mov r0, #0
+    ldr r1, addr_col
+    mov r2, #1
+    bl read
+
+    subr r0, r0, #48 /* convert char to int */
+    ldr r1, addr_board
+
+    while:
+    bl move_valid
+    cmp r0, 1
+    beq add_checker
+
+    /*prompt for move
+     *remind of invalid move
+     */
+    b while
+
+    /* add checker specified in row mayor order */
+    add_checker:
+        
+    bx lr
+
+.global check_board
+.func check_board
+
+/* Go though board check if there are 4 consecutive
+ * checkers, check for diagonal.
+ * return 0 for non-final status, 
+ * return 1 for win
+ * return 2 for tie
+ */
+check_board:
 
     bx lr
 
 .global main
-
 .func main
-
-/* -- Code Section */
 
 main:
     
@@ -47,7 +100,6 @@ main:
      * 1 represents win
      * 2 represents tie
      */
-
     mov r4, #0
 
     /* Set initial player */
@@ -90,11 +142,12 @@ main:
     b while
 
     out:
-        cmp 2
-        beq tie /* else is win */
-        mov r0, #1
+        cmp 2 /*branch if tie */
+        beq tie 
+
+        mov r0, #1 /* else if win */
         ldr r1, addr_win
-        
+
         b end
 
     tie:
@@ -117,10 +170,10 @@ main:
         bx lr
 
 /* Labels to access data */
-
 addr_board: .word board
 addr_invalid_move: .word invalid_move
 addr_move_prompt: .word move_prompt
+addr_col: .word col
 addr_win: .word win
 addr_tie: .word tie
 addr_exiting: .word exiting
